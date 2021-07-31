16b2e2f52792ca4c430cb3477180469e0523c587
Sarah Loutrel <s.loutrel@gmail.com>
2021-07-30 20:30:24 -0700

    Enable Multiplayer Rendering

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,30 +1,110 @@
 import React, { Component } from "react";
 import GameForm from "./GameForm";
 import Player from "./Player";
+const _ = require("lodash");
 
 class GameBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newGame: [],
+      game: [],
     };
     this.create = this.create.bind(this);
+    // this.allPlayers = this.allPlayers.bind(this);
   }
 
   create(newGameDetails) {
-    this.setState({
-      newGame: newGameDetails,
-    });
+    this.setState({ game: newGameDetails });
   }
+
+  //   allPlayers(player) {
+  //     <Player
+  //       lives={players[key].lives}
+  //       key={players[key].id}
+  //       id={players[key].id}
+  //       name={players[key].name}
+  //     />;
+  //   }
+
+  //     const gameSetup = this.state.game;
+  //     const players = gameSetup.players;
+  //     for (let key in players) {
+  //       if (players.hasOwnProperty(key)) {
+  //         return (
+  //           <Player
+  //             lives={players[key].lives}
+  //             key={players[key].id}
+  //             id={players[key].id}
+  //             name={players[key].name}
+  //           />
+  //         );
+  //       }
+  //     }
+  //   }
   render() {
-    //     const players = this.state.newGame.map((player) => {
-    //       return <Player lives={player.lives} key={player.id} id={player.id} />;
-    //     });
+    const game = this.state.game;
+    const players = game.players;
+    const allPlayers = _.map(players, (player, i) => {
+      //     this.state.game, (e) => {
+      //   e.players.map((player) => {
+      return (
+        <Player
+          lives={player.lives}
+          key={i}
+          id={player.id}
+          name={player.name}
+          gameOver={player.gameOver}
+        />
+      );
+    });
+
+    // const allPlayers = this.state.game.map((p, index) => {
+    //   this.return(
+    //     <div key={index}>
+    //       {p.players.map((player, i) => (
+    //         <Player
+    //           lives={player.lives}
+    //           key={player.id}
+    //           id={player.id}
+    //           name={player.name}
+    //         />
+    //       ))}
+    //     </div>
+    //   );
+    // });
+    // const allPlayers = this.state.game.map((p) => {
+    //   const player = this.state.players[p];
+
+    //   return (
+    //     <Player
+    //       lives={player.lives}
+    //       key={p}
+    //       id={player.id}
+    //       name={player.name}
+    //     />
+    //   );
+    // });
+
+    // let allPlayers = Object.keys(this.state.game).map((playerKey, i) => {
+    //   const playerData = this.state.game[playerKey].players;
+
+    //   return Object.keys(playerData).map((player) => {
+    //     return (
+    //       <Player
+    //         key={player.id}
+    //         id={player.id}
+    //         name={player.name}
+    //         lives={player.lives}
+    //       />
+    //     );
+    //   });
+    // });
+
     return (
       <div>
         <GameForm createGame={this.create} />
-        <Player name="Sarah" lives={this.state.newGame.lives} />
-        {/* <div className="Players">{players}</div> */}
+
+        <div className="Players">{allPlayers}</div>
         <button>Rematch</button>
         <button>New Battle</button>
         <a
--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,48 +1,82 @@
 import React, { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
+const rangeInclusive = require("range-inclusive");
 
 class GameForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { lives: "20", playerCount: "2" };
+    this.state = {
+      players: [
+        { name: "Player1", lives: 20, id: 1, gameOver: false },
+        { name: "Player2", lives: 20, id: 2, gameOver: false },
+      ],
+      playerCount: 2,
+      lives: 20,
+      isEditing: true,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleSubmit(evt) {
     evt.preventDefault();
-    this.props.createGame({ ...this.state, id: uuidv4(), gameOver: false });
+    if (this.state.playerCount > 2) {
+      let playerNames = [];
+      let players = rangeInclusive(1, this.state.playerCount, 1);
+      for (let i = 0; i < players.length; i++) {
+        let playerData = `name: "Player${players[i]}", lives: ${
+          this.state.lives
+        }, id: ${uuidv4()}, gameOver: ${false}`;
+        playerNames.push({ playerData });
+      }
+      this.setState({
+        players: [...playerNames],
+      });
+    }
+    this.handleEdit();
+    this.props.createGame({ ...this.state });
+  }
+
+  handleEdit() {
+    this.setState({ isEditing: !this.state.isEditing });
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Number in the gathering:
-          <input
-            type="number"
-            min="2"
-            value={this.state.playerCount}
-            onChange={(e) => {
-              this.setState({ playerCount: e.target.value });
-            }}
-          ></input>
-        </label>
-        <label>
-          Select your starting life value:{" "}
-          <input
-            type="number"
-            max="40"
-            min="10"
-            step="10"
-            value={this.state.lives}
-            onChange={(e) => {
-              this.setState({ lives: e.target.value });
-            }}
-          ></input>
-        </label>
-        <button>Let's battle!</button>
-      </form>
-    );
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Number in the gathering:
+            <input
+              type="number"
+              min="2"
+              value={this.state.playerCount}
+              onChange={(e) => {
+                this.setState({ playerCount: e.target.value });
+              }}
+            ></input>
+          </label>
+          <label>
+            Select your starting life value:{" "}
+            <input
+              type="number"
+              max="40"
+              min="10"
+              step="10"
+              value={this.state.lives}
+              onChange={(e) => {
+                this.setState({ lives: e.target.value });
+              }}
+            ></input>
+          </label>
+          <button>Let's battle!</button>
+        </form>
+      );
+    } else {
+      result = <button onClick={this.handleEdit}>Game Details</button>;
+    }
+    return result;
   }
 }
 
